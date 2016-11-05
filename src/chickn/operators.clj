1364dc0572c2bb347300a7c0fdcb51e1a594d55f
Kostas Georgiadis <kongeor@gmail.com>
2016-11-05 15:17:02 +0200

    WIP operator extraction

--- src/chickn/operators.clj
+++ src/chickn/operators.clj
@@ -1,23 +1,30 @@
 (ns chickn.operators
   (:require [clojure.spec :as s]))
 
+(defn noop [& args])
+
 ; -----
 ; Spec
 
 (s/def ::rate-num (s/double-in :min 0 :max 1 :NaN false :infinite? false))
 
-(s/def :crossover/type keyword?)
-(s/def :crossover/rate ::rate-num)
-(s/def :crossover/pointcuts integer?)
+(s/def ::type keyword?)
+(s/def ::rate ::rate-num)
+(s/def ::elitism ::rate-num)
+(s/def ::pointcuts integer?)
+(s/def ::random-point ifn?)                                 ;; a func accepting a chromosome return a random position
 
-(defmulti crossover-type :crossover/type)
-(defmethod crossover-type :crossover/order-crossover [_]
-  (s/keys :req [:crossover/type :crossover/rate]))
+(s/def ::selector ifn?)                                     ; FIXME should live in it's own namespace
 
-(defmethod crossover-type :crossover/cut-crossover [_]
-  (s/keys :req [:crossover/type :crossover/rate :crossover/pointcuts]))
+(defmulti operator-type ::type)
 
-(s/def :crossover/crossover (s/multi-spec crossover-type :crossover/type))
+(defmethod operator-type ::order-crossover [_]
+  (s/keys :req [::type ::rate ::elitism ::selector ::random-point]))
+
+(defmethod operator-type ::cut-crossover [_]
+  (s/keys :req [::type ::rate ::pointcuts ::elitism ::selector ::random-point]))
+
+(s/def ::operator (s/multi-spec operator-type ::type))
 
 ; -----
 ; Genetic Operators
@@ -30,14 +37,55 @@
       [o1 o2])))
 
 
+; ----
+; constructor funcs
+
+;; FIXME: remove
+(defn make-selector [cfg])
+
+(defn crossover-pop [{:keys [::selector ::random-func ::rate ::elitism ::crossover]}]
+  (let [selector (make-selector selector)]
+    (fn [pop]
+      (let [pop-size (-> pop :pop count)
+            new-gen (map-indexed
+                      (fn [i parent]
+                        (if (and (> rate (random-func)) (>= (/ i pop-size) elitism))
+                          (let [other (selector pop)]
+                            (first (crossover parent other)))
+                          parent)) (:pop pop))]
+        (assoc-in pop [:pop] new-gen)))))
+
+
+(defmulti operator ::type)
+(defmethod operator ::cut-crossover [cfg]
+  (crossover-pop cfg))
+
+(operator {::type ::cut-crossover
+           ::rate 0.3
+           ::elitism 0.1
+           ::pointcuts 1
+           ::selector noop})
+
+
+
+#_(defn breed-pop [{:keys [random-func crossover-rate crossover elitism-rate] :as cfg} pop]
+  (let [pop-size (-> pop :pop count)
+        new-gen (map-indexed
+                  (fn [i parent]
+                    (if (and (> crossover-rate (random-func)) (>= (/ i pop-size) elitism-rate))
+                      (let [other (roulette cfg pop)]
+                        (first (crossover parent other)))
+                      parent)) (:pop pop))]
+    (assoc-in pop [:pop] new-gen)))
+
 ; ------
 ; Playground
 
-(s/conform :crossover/crossover
+#_(s/conform :crossover/crossover
            {:crossover/type :crossover/cut-crossover
             :crossover/rate 0.3
             :crossover/pointcuts 1})
 
-(s/conform :crossover/crossover
+#_(s/conform :crossover/crossover
            {:crossover/type :crossover/order-crossover
             :crossover/rate 0.3})
