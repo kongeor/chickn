36dc1a7356b173272d1d5a678fa6e3fe569d5a68
Kostas Georgiadis <kongeor@gmail.com>
2017-11-04 14:54:59 +0200

    Update description and url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject chickn "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "clojure(script) genetic algorithms toolkit"
+  :url "https://github.com/kongeor/chickn"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-beta2"]
